200a2a91746bfa64c3ca615f530a76759d14eb4a
KevinJappert <jappertkevin@gmail.com>
2023-09-21 23:08:51 -0300

    Limpiando codigo

--- src/Funciones/funciones.ts
+++ src/Funciones/funciones.ts
@@ -1,7 +1,150 @@
+import { v4 as uuidv4 } from 'uuid';
+import { listaTareaResultado, tareaInput } from '../main';
 export interface Tarea {
     id: string;
     texto: string;
 }
+
+// Variable para controlar si se ha realizado una edición
+export let seRealizoEdicion: boolean = false;
+
+const mensajeParrafo = document.getElementById("mensaje");
+
+
+// Función para agregar una tarea a la lista
+export function agregarTareaALista(tareaInput: string, tareaId: string, listaTareaResultado: HTMLElement) {
+    tareaInput = tareaInput.trim();
+
+    const tareasEnLista = Array.from(listaTareaResultado!.children);
+
+    const tareaExistente = tareasEnLista.find((tarea) => tarea.getAttribute('data-id') === tareaId);
+
+    if (!tareaExistente && tareaInput !== '') {
+        const nuevaTarea = document.createElement("li");
+        nuevaTarea.classList.add("tareaAsignada");
+
+        // Asignar el identificador único a la tarea
+        nuevaTarea.setAttribute("data-id", tareaId);
+
+        const tareaTexto = document.createElement('span');
+        tareaTexto.textContent = tareaInput;
+
+        const accionesContainer = document.createElement('div');
+        accionesContainer.className = 'flex items-center';
+
+        const inputChequeo = document.createElement('input');
+        inputChequeo.type = 'checkbox';
+        inputChequeo.className = 'w-4 h-4  mr-2 mt-3 text-blue-500';
+
+        let tareaResuelta = false;
+
+        inputChequeo.addEventListener('change', () => {
+            tareaResuelta = inputChequeo.checked;
+
+            if (tareaResuelta) {
+                nuevaTarea.classList.add('tareaResuelta');
+            } else {
+                nuevaTarea.classList.remove('tareaResuelta');
+            }
+
+            // Guardar el estado del checkbox en el almacenamiento local
+            guardarEstadoCheckboxEnLocalStorage(tareaId, tareaResuelta);
+        });
+
+        const imgEliminar = document.createElement('img');
+        imgEliminar.src = './public/img/eliminar.png';
+        imgEliminar.alt = 'Eliminar';
+        imgEliminar.className = 'accion-eliminar w-8 h-8 mt-3 mr-2 cursor-pointer';
+
+        imgEliminar.addEventListener('click', () => {
+            console.log('Haz hecho clic en el icono de eliminar');
+            eliminarTareaListaYLocalStorage(tareaId);
+        });
+
+        const imgEditar = document.createElement('img');
+        imgEditar.src = './public/img/boligrafo.png';
+        imgEditar.alt = 'Editar';
+        imgEditar.className = 'accion-editar w-8 h-8 mt-3 cursor-pointer';
+
+        const inputEditar = document.createElement('input');
+        inputEditar.type = 'text';
+        inputEditar.className = 'hidden';
+
+        inputEditar.addEventListener('keydown', (event) => {
+            if (event.key === 'Enter') {
+                actualizarTextoTarea(inputEditar, tareaTexto, tareaId);
+            }
+        });
+
+        imgEditar.addEventListener('click', () => {
+            console.log('Haz hecho clic en el icono de editar');
+            habilitarEdicionTarea(inputEditar, tareaTexto);
+        });
+
+        // Edición de una tarea
+        inputEditar.addEventListener('blur', () => {
+            actualizarTextoTarea(inputEditar, tareaTexto, tareaId);
+        });
+
+        accionesContainer.appendChild(inputChequeo);
+        accionesContainer.appendChild(imgEliminar);
+        accionesContainer.appendChild(imgEditar);
+
+        nuevaTarea.appendChild(tareaTexto);
+        nuevaTarea.appendChild(accionesContainer);
+        nuevaTarea.appendChild(inputEditar);
+
+        listaTareaResultado.appendChild(nuevaTarea);
+
+        // Verifica si el estado del checkbox está guardado en el almacenamiento local
+        if (inputChequeo && tareaId) {
+            const estadoGuardado = obtenerEstadoCheckboxDesdeLocalStorage(tareaId);
+            if (estadoGuardado !== null) {
+                inputChequeo.checked = estadoGuardado;
+                actualizarEstadoTarea(tareaId, estadoGuardado);
+            }
+        }
+    }
+}
+
+// Agregar evento de clic al botón "Asignar"
+export function agregarTarea() {
+    if (tareaInput && !seRealizoEdicion && mensajeParrafo && listaTareaResultado) {
+        const tareaValor = tareaInput.value ? tareaInput.value.trim() : '';
+
+        // Mostrar un mensaje de error si la tarea es inválida
+        mensajeParrafo.textContent = "Por favor, ingrese una tarea válida.";
+
+        if (tareaValor === "" || !isNaN(Number(tareaValor))) {
+            // Limpiar el mensaje de error después de 2 segundos
+            setTimeout(() => {
+                mensajeParrafo.textContent = "";
+            }, 2000);
+        } else {
+            mensajeParrafo.textContent = "";
+            console.log(tareaValor);
+
+            // Verificar si la tarea ya existe en la lista antes de agregarla
+            const tareasEnLista = Array.from(listaTareaResultado.children);
+            const tareaExistente = tareasEnLista.some((tarea) => tarea.textContent === tareaValor);
+
+            if (!tareaExistente) {
+                // Crear un objeto Tarea con el texto
+                const nuevaTarea: Tarea = {
+                    id: uuidv4(),
+                    texto: tareaValor,
+                };
+
+                agregarTareaALista(tareaValor, nuevaTarea.id, listaTareaResultado);
+                actualizarSeRealizoEdicion(false);
+                tareaInput.value = "";
+
+                // Llamar a guardarTareaLocalStorage con el objeto Tarea
+                guardarTareaLocalStorage(nuevaTarea);
+            }
+        }
+    }
+};
 
 // Función para guardar una tarea en el almacenamiento local
 export function guardarTareaLocalStorage(tarea: Tarea) {
@@ -76,8 +219,51 @@ export function actualizarTextoTarea(inputEditar: HTMLInputElement, tareaTexto: 
     editarTareaLocalStorage(tareaId, inputEditar.value);
 }
 
-// Variable para controlar si se ha realizado una edición
-export let seRealizoEdicion: boolean = false;
+// Función para guardar el estado del checkbox en el almacenamiento local
+export function guardarEstadoCheckboxEnLocalStorage(tareaId: string, estado: boolean) {
+    const estados = obtenerEstadosCheckboxesDesdeLocalStorage();
+    estados[tareaId] = estado;
+    localStorage.setItem('estadosCheckboxes', JSON.stringify(estados));
+}
+
+// Función para obtener el estado del checkbox desde el almacenamiento local
+export function obtenerEstadoCheckboxDesdeLocalStorage(tareaId: string): boolean | null {
+    const estados = obtenerEstadosCheckboxesDesdeLocalStorage();
+    return estados[tareaId] || null;
+}
+
+// Función para obtener todos los estados de los checkboxes desde el almacenamiento local
+export function obtenerEstadosCheckboxesDesdeLocalStorage(): { [key: string]: boolean } {
+    const estadosString = localStorage.getItem('estadosCheckboxes');
+    return estadosString ? JSON.parse(estadosString) : {};
+}
+
+// Función para actualizar el estado de la tarea
+export function actualizarEstadoTarea(tareaId: string, estado: boolean) {
+    const tarea = document.querySelector(`[data-id="${tareaId}"]`);
+    if (tarea) {
+        if (estado) {
+            tarea.classList.add('tareaResuelta');
+        } else {
+            tarea.classList.remove('tareaResuelta');
+        }
+    }
+}
+
+// Restaurar el estado de los checkboxes cuando se carga la página
+export function restaurarEstadoCheckboxes() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+        const tareaId = checkbox.parentElement?.parentElement?.getAttribute('data-id');
+        if (tareaId) {
+            const estadoGuardado = obtenerEstadoCheckboxDesdeLocalStorage(tareaId);
+            if (estadoGuardado !== null) {
+                (checkbox as HTMLInputElement).checked = estadoGuardado;
+                actualizarEstadoTarea(tareaId, estadoGuardado);
+            }
+        }
+    });
+}
 
 export function actualizarSeRealizoEdicion(valor: boolean) {
     seRealizoEdicion = valor;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { v4 as uuidv4 } from 'uuid';
-import { eliminarTareaListaYLocalStorage, cargarTareasDesdeLocalStorage, guardarTareaLocalStorage, habilitarEdicionTarea, Tarea, actualizarTextoTarea, seRealizoEdicion, actualizarSeRealizoEdicion } from "./Funciones/funciones";
+// Importa tus módulos y funciones necesarios aquí...
+import {
+    cargarTareasDesdeLocalStorage,
+    restaurarEstadoCheckboxes,
+    agregarTareaALista,
+    agregarTarea,
+} from "./Funciones/funciones";
 
 // Obtener elementos del DOM
 const btnAsignar = document.getElementById("boton-asignar");
-const tareaInput = document.getElementById("tarea") as HTMLInputElement;
-const mensajeParrafo = document.getElementById("mensaje");
-const listaTareaResultado = document.getElementById("lista-tarea");
+export const tareaInput = document.getElementById("tarea") as HTMLInputElement;
+export const listaTareaResultado = document.getElementById("lista-tarea");
 
 // Evento DOMContentLoaded para cargar tareas desde el LocalStorage
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,94 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
     tareasGuardadas.forEach((tareaGuardada) => {
         agregarTareaALista(tareaGuardada.texto, tareaGuardada.id, listaTareaResultado!);
     });
+
+    // Restaurar el estado de los checkboxes cuando se carga la página
+    restaurarEstadoCheckboxes();
 });
-
-
-// Función para agregar una tarea a la lista
-function agregarTareaALista(tareaInput: string, tareaId: string, listaTareaResultado: HTMLElement) {
-    tareaInput = tareaInput.trim();
-
-    const tareasEnLista = Array.from(listaTareaResultado!.children);
-
-    const tareaExistente = tareasEnLista.find((tarea) => tarea.getAttribute('data-id') === tareaId);
-
-    if (!tareaExistente && tareaInput !== '') {
-        const nuevaTarea = document.createElement("li");
-        nuevaTarea.classList.add("tareaAsignada");
-
-        // Asignar el identificador único a la tarea
-        nuevaTarea.setAttribute("data-id", tareaId);
-
-        const tareaTexto = document.createElement('span');
-        tareaTexto.textContent = tareaInput;
-
-        const accionesContainer = document.createElement('div');
-        accionesContainer.className = 'flex items-center';
-
-        const inputChequeo = document.createElement('input');
-        inputChequeo.type = 'checkbox';
-        inputChequeo.className = 'w-4 h-4  mr-2 mt-3 text-blue-500';
-
-        let tareaResuelta = false;
-
-        inputChequeo.addEventListener('change', () => {
-            tareaResuelta = inputChequeo.checked;
-
-            if (tareaResuelta) {
-                nuevaTarea.classList.add('tareaResuelta');
-            } else {
-                nuevaTarea.classList.remove('tareaResuelta');
-            }
-        });
-
-        const imgEliminar = document.createElement('img');
-        imgEliminar.src = './public/img/eliminar.png';
-        imgEliminar.alt = 'Eliminar';
-        imgEliminar.className = 'accion-eliminar w-8 h-8 mt-3 mr-2 cursor-pointer';
-
-        imgEliminar.addEventListener('click', () => {
-            console.log('Haz hecho clic en el icono de eliminar');
-            eliminarTareaListaYLocalStorage(tareaId);
-        });
-
-        const imgEditar = document.createElement('img');
-        imgEditar.src = './public/img/boligrafo.png';
-        imgEditar.alt = 'Editar';
-        imgEditar.className = 'accion-editar w-8 h-8 mt-3 cursor-pointer';
-
-        const inputEditar = document.createElement('input');
-        inputEditar.type = 'text';
-        inputEditar.className = 'hidden';
-
-        inputEditar.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                actualizarTextoTarea(inputEditar, tareaTexto, tareaId);
-            }
-        });
-
-        imgEditar.addEventListener('click', () => {
-            console.log('Haz hecho clic en el icono de editar');
-            habilitarEdicionTarea(inputEditar, tareaTexto);
-        });
-
-        // Edición de una tarea
-        inputEditar.addEventListener('blur', () => {
-            actualizarTextoTarea(inputEditar, tareaTexto, tareaId);
-        });
-
-        accionesContainer.appendChild(inputChequeo);
-        accionesContainer.appendChild(imgEliminar);
-        accionesContainer.appendChild(imgEditar);
-
-        nuevaTarea.appendChild(tareaTexto);
-        nuevaTarea.appendChild(accionesContainer);
-        nuevaTarea.appendChild(inputEditar);
-
-        listaTareaResultado.appendChild(nuevaTarea);
-
-
-    }
-}
 
 // Agregar evento de clic al botón "Asignar"
 btnAsignar?.addEventListener('click', () => {
@@ -113,47 +33,6 @@ tareaInput?.addEventListener('keydown', (event) => {
         agregarTarea();
     }
 });
-
-// Agregar evento de clic al botón "Asignar"
-function agregarTarea() {
-    if (tareaInput && !seRealizoEdicion && mensajeParrafo && listaTareaResultado) {
-        const tareaValor = tareaInput.value ? tareaInput.value.trim() : '';
-
-        // Mostrar un mensaje de error si la tarea es inválida
-        mensajeParrafo.textContent = "Por favor, ingrese una tarea válida.";
-
-        if (tareaValor === "" || !isNaN(Number(tareaValor))) {
-            // Limpiar el mensaje de error después de 2 segundos
-            setTimeout(() => {
-                mensajeParrafo.textContent = "";
-            }, 2000);
-        } else {
-            mensajeParrafo.textContent = "";
-            console.log(tareaValor);
-
-            // Verificar si la tarea ya existe en la lista antes de agregarla
-            const tareasEnLista = Array.from(listaTareaResultado.children);
-            const tareaExistente = tareasEnLista.some((tarea) => tarea.textContent === tareaValor);
-
-            if (!tareaExistente) {
-                // Crear un objeto Tarea con el texto
-                const nuevaTarea: Tarea = {
-                    id: uuidv4(),
-                    texto: tareaValor,
-                };
-
-                agregarTareaALista(tareaValor, nuevaTarea.id, listaTareaResultado);
-                actualizarSeRealizoEdicion(false);
-                tareaInput.value = "";
-
-                // Llamar a guardarTareaLocalStorage con el objeto Tarea
-                guardarTareaLocalStorage(nuevaTarea);
-            }
-        }
-    }
-};
-
-
 
 // Resto de tu código...
 
